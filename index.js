c124e043aee61fa9d6943d8f52f4a361eedb2e94
Tess <tess.mueske@gmail.com>
2023-11-26 15:54:37 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random() * 10); 
+
+let num3 = 12
+let num4 = 3 
+const mod = num3 / num4 
+
+const max = Math.max(1, 3, 5, 7, 9, 20)
